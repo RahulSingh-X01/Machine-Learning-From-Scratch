aa72ca6a998be9a163fc1af8c0c4c9e06da79bb0
Rahul Singh <rahulxsingh2005@gmail.com>
2026-03-23 17:12:43 +0530

    Add gradient descent fucntion

--- logistic-regression-from-scratch/logistic_regression_model.py
+++ logistic-regression-from-scratch/logistic_regression_model.py
@@ -16,6 +16,24 @@ def loss_function(w, b, points):
     
     return binary_cross_entropy
 
-
+#----------------Gradeint Descent------------#
+def gradient_desent(w, b, L, points):
+    points = np.array(points)
+    m = len(points)
+    
+    x = points[:, :-1]
+    y = points[:, -1]
+    
+    y_pred =  1/(1+np.exp(-(np.dot(x, w)+b)))
+    
+    error = y_pred - y
+    
+    dJ_dw = (1/m)*np.dot(x.T, error)
+    dJ_db = (1/m)*np.sum(error)
+    
+    w = w - L * dJ_dw
+    b = b - L * dJ_db
+    
+    return w, b
 
 
